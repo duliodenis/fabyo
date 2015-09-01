7953e6abdc9b178bd402b056e516f217c9950a87
Dulio Denis <dulio.denis@yahoo.com>
2015-09-01 09:20:43 -0400

    Added a TableView for LibriVox API call results

--- Fabyo/Fabyo/ViewController.swift
+++ Fabyo/Fabyo/ViewController.swift
@@ -8,10 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var library: JSON = JSON.nullJSON
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        booksLookup()
+    }
+
+
+    func booksLookup() {
+        let urlPath = NSURL(string: "https://librivox.org/api/feed/audiobooks/?format=json")!
+        
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(urlPath) {
+            data, response, error -> Void in
+            
+            if ((error) != nil) {
+                print(error.localizedDescription)
+            }
+            
+            var jsonError : NSError?
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as! NSDictionary
+            
+            if ((jsonError) != nil) {
+                print(jsonError!.localizedDescription)
+            }
+            // update the UITableView on the main thread
+            dispatch_async(dispatch_get_main_queue()) {
+                self.library = JSON(data: data)
+                
+                let numberOfBooks = self.library["books"].count
+                print("Results are \(numberOfBooks) books\n")
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
+    
+    
+    // MARK: - TableView Delegte Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let books = library["books"]
+        return books.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let row = indexPath.row
+        if let bookName = library["books"][row]["title"].string as String! {
+            cell.textLabel?.text = bookName
+        }
+        
+        if let authorFirstName = library["books"][row]["authors"][0]["first_name"].string as String! {
+            if let authorLastName = library["books"][row]["authors"][0]["last_name"].string as String! {
+                cell.detailTextLabel?.text = "\(authorFirstName) \(authorLastName)"
+            }
+        }
+        
+        return cell
     }
 
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Fabyo
-Free Audio Books, Yo - Fabyo is an iOS App written in Swift that provides over 9,000 free public domain audio books through the Librivox API.
+Free Audio Books, Yo - Fabyo is an iOS App written in Swift that provides over 9,000 free public domain audio books through [Librivox](https://librivox.org/) and it's API.
+
+LibriVox volunteers record chapters of books in the public domain, and then they release the audio files back onto the net for free. If you'd like to help them please visit their [how to donate](https://librivox.org/pages/how-to-donate/) page.
 
 ### Licensing
 Fabyo is licensed under [the MIT License](https://github.com/duliodenis/fabyo/blob/master/LICENSE).
